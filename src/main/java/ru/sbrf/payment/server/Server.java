e5043f426df594cc205cf9aaf82b3c0d064ce1f2
rvkhlystov <rvkhlystov@gmail.com>
2020-09-14 21:19:14 +0300

    Вынес из класса Server в отдельные классы базы данных клиентов и платежей. Пока не реализовано: 1) логирование в случае, если все проверки не пройдены; 2) тесты к классам

--- src/main/java/ru/sbrf/payment/server/Server.java
+++ src/main/java/ru/sbrf/payment/server/Server.java
@@ -1,8 +1,6 @@
 package ru.sbrf.payment.server;
 
 import lombok.Getter;
-import ru.sbrf.payment.client.Account;
-import ru.sbrf.payment.client.User;
 import ru.sbrf.payment.common.DescriptionStatusPayment;
 import ru.sbrf.payment.common.Payment;
 import ru.sbrf.payment.common.exceptions.BusinessExceptions;
@@ -10,7 +8,6 @@ import ru.sbrf.payment.server.check.CheckAccount;
 import ru.sbrf.payment.server.check.CheckUser;
 
 import java.util.Date;
-import java.util.HashMap;
 
 @Getter
 
@@ -21,29 +18,24 @@ public class Server implements ServerInterface {
     private int port = 443; //номер порта для взаимодействия с приложением
     private String protocol = "https"; //название протокола взаимодействия
 
-    private HashMap<String, User> clients = new HashMap<>();
-    private HashMap<Integer, Payment> payments = new HashMap<>();
 
     private int numberOperationServer = 0;
     private boolean statusOperation = true;
 
-    public void addClient (String clientNumber, User client) {
-        clients.put(clientNumber, client);
-    }
 
     @Override
-    public Payment makePayment(Payment payment) throws BusinessExceptions {
+    public Payment makePayment(Payment payment, DataBaseClients dataBaseClients, DataBasePayments dataBasePayments) throws BusinessExceptions {
         numberOperationServer += 1;
 
         //получаем номер клиента из платежки
         String clientNumberFromPayment = payment.getClientNumber();
         //проверяем,есть ли такой клиент в базе
-        statusOperation &= CheckUser.checkUser(clientNumberFromPayment, clients);
+        statusOperation &= CheckUser.checkUser(clientNumberFromPayment, dataBaseClients.getClients());
 
         //получаем номер счета клиента из платежки
         long accountNumberPayment = payment.getAccountNumber();
         //получаем номер счета клиента из базы
-        long accountNumberUser = clients.get(payment.getClientNumber()).getAccount().getAccountNumber();
+        long accountNumberUser = dataBaseClients.getClients().get(payment.getClientNumber()).getAccount().getAccountNumber();
         //проверяем корректность указанного пользователем счета
         statusOperation &= CheckAccount.checkAccountNumber(accountNumberUser,accountNumberPayment);
 
@@ -51,16 +43,16 @@ public class Server implements ServerInterface {
         float amount = payment.getAmount();
 
         //получаем баланс счета
-        float balance = clients.get(payment.getClientNumber()).getAccount().getBalance();
+        float balance = dataBaseClients.getClients().get(payment.getClientNumber()).getAccount().getBalance();
 
         //проверяем достаточность средств на счете
         statusOperation &= CheckAccount.checkBalanceForMakeOperation(balance, amount);
 
         //меняем баланс
-        clients.get(payment.getClientNumber()).getAccount().setBalance(balance - amount);
+        dataBaseClients.getClients().get(payment.getClientNumber()).getAccount().setBalance(balance - amount);
 
 
-        //Добавляем платеж в базу
+        //Заполняем поля на стороне сервера
         //Необходимо пересмотреть логику, чтобы реализовать возможность записи в базу,
         //если не пройдены проверки корректности данных в платеже
         DescriptionStatusPayment descriptionStatusPayment = DescriptionStatusPayment.success; //после добавления проверок исправить
@@ -69,7 +61,8 @@ public class Server implements ServerInterface {
         payment.setStatusOperation(statusOperation);
         payment.setDescriptionStatusPayment(descriptionStatusPayment);
 
-        payments.put(numberOperationServer, payment);
+        //Добавляем платеж в базу
+        dataBasePayments.addPayment(payment);
         return payment;
     }
 
